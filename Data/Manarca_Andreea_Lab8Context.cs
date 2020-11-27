6284b99fd5cc90e0e2e2da86307febb7fd325244
Manarca Andreea <a.manarca@yahoo.com>
2020-11-27 19:01:26 +0200

    Add project files.

---
+++ Data/Manarca_Andreea_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Manarca_Andreea_Lab8.Models;
+
+namespace Manarca_Andreea_Lab8.Data
+{
+    public class Manarca_Andreea_Lab8Context : DbContext
+    {
+        public Manarca_Andreea_Lab8Context (DbContextOptions<Manarca_Andreea_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Manarca_Andreea_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Manarca_Andreea_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Manarca_Andreea_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Manarca_Andreea_Lab8.Data;
+using Manarca_Andreea_Lab8.Models;
+
+namespace Manarca_Andreea_Lab8.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Manarca_Andreea_Lab8.Data.Manarca_Andreea_Lab8Context _context;
+
+        public IndexModel(Manarca_Andreea_Lab8.Data.Manarca_Andreea_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Category> Category { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Category = await _context.Category.ToListAsync();
+        }
+    }
+}
